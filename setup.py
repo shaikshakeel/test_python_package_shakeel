4229bc0f93ca36a563ce338c01c346397d7b5305
shakeel <shaik.shakeel@freshworks.com>
2018-10-09 19:19:09 +0530

    inital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_python_package_shakeel',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/shaikshakeel/test_python_package_shakeel',
+      author='shakeel',
+      author_email='shaik.shakeel@freshworks.com',
+      license='MIT',
+      packages=['test_python_package_shakeel'],
+      zip_safe=False)
